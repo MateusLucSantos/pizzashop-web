66ae0709621b27a1528ca920bfc998790f24fab5
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-03-31 16:35:22 -0300

    Revisão nos botões de paginação

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -28,19 +28,19 @@ export function Pagination({
           Página {pageIndex + 1} de {pages}
         </div>
         <div className="flex items-center gap-2">
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-4">
             <ChevronsLeft className="h-4 w-4" />
             <span className="sr-only">Primeira página</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-4">
             <ChevronLeft className="h-4 w-4" />
             <span className="sr-only">Próxima anterior</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-4">
             <ChevronRight className="h-4 w-4" />
             <span className="sr-only">Próxima página</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-4">
             <ChevronsRight className="h-4 w-4" />
             <span className="sr-only">Última página</span>
           </Button>
